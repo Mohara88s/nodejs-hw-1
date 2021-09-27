b4f08717450254841a1fb5b680cd4674637ff7a7
Mohara88s <73835861+Mohara88s@users.noreply.github.com>
2021-09-27 15:05:38 +0300

    add contacts functions and testing them

--- contacts.js
+++ contacts.js
@@ -1,7 +1,12 @@
-const fs = require("fs/promises");
+const fs = require('fs').promises;
 const path = require("path")
 
 const contactsPath = path.join(__dirname, "db", "contacts.json");
+
+const updateContacts = async (contacts) => {
+  const newContacts = await fs.writeFile(contactsPath, JSON.stringify(contacts));    
+  return newContacts;
+}
 
 const listContacts = async() => {
   const data = await fs.readFile(contactsPath);
@@ -9,22 +14,45 @@ const listContacts = async() => {
   return contacts;
  }
   
-  // function getContactById(contactId) {
+  const getContactById = async(contactId) => {
+    const contacts = await listContacts();
+    const idx = contacts.findIndex(item=>item.id === contactId);
+    if(idx === -1) {
+      return null;
+    }
+    return contacts[idx];
+  }
+  
+  const removeContact = async(contactId) => {
+    const contacts = await listContacts();
+    const idx = contacts.findIndex(item=>item.id === contactId);
+    if(idx === -1) {
+        return null;
+    }
+    contacts.splice(idx, 1);
+    await updateContacts(contacts);
+    return true;   
+  }
+  
+  const addContact = async(data) => {
+    const contacts = await listContacts();
+    const idx = contacts.findIndex(item=>item.name === data.name);
+    if(idx !== -1) {
+      throw new Error(`Contact with name ${data.name} exists in contact list`);;
+    }
+    const maxId = Math.max.apply(null, contacts.map(item => item.id));
+    const newId = maxId + 1;
+    const newContact = { id: newId, ...data };
+    contacts.push(newContact);
+    await updateContacts(contacts);  
+    return newContact;
+  }
 
-  // }
-  
-  // function removeContact(contactId) {
-   
-  // }
-  
-  // function addContact(name, email, phone) {
-    
-  // }
   const contactsFunctions = {
     listContacts,
-    // getContactById,
-    // removeContact,
-    // addContact
+    getContactById,
+    removeContact,
+    addContact
   };
 
 module.exports = contactsFunctions;
--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 const contactsOps = require("./contacts")
+const fs = require('fs').promises;
 
 (async()=> {
     try {
         const contacts = await contactsOps.listContacts();
-        console.log(contacts)
+        console.log(`Contact list \n`, contacts)
+
+        const id = 1
+
+        const contactById = await contactsOps.getContactById(id);
+        if (!contactById){
+            throw new Error(`Contact with id=${id} not found`);
+        } 
+        console.log(`Contact with id=${id} \n`, contactById)
+
+
+        const result = await contactsOps.removeContact(id);
+        if (!result){
+            throw new Error(`Contact with id=${id} not found`);
+        }
+        console.log(`Successfully remove contact with id=${id}`);
+
+        const newData = {
+            "name": "Allena Raymonda",
+            "email": "nullaa.ante@vestibul.co.uk",
+            "phone": "(992) 914-3792"
+          };
+          const newAddedContact = await contactsOps.addContact(newData)
+          console.log(`Successfully added new contact \n`, newAddedContact);
+          
+
+
     }
     catch (error) {
         console.log(error.message)
     }
 })();
-console.log('hallo')
